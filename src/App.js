9e3c687ff29ddc67e60c99b2b8ba9e8f6d5a7a85
dhruvinparikh93 <imsilent93@gmail.com>
2020-04-08 13:38:32 -0400

    test husky

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import React from "react";
-import "./App.css";
+import React from 'react';
+import './App.css';
 
 function App() {
   return (
@@ -14,7 +14,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+    Learn React
         </a>
         <p>Start adding here</p>
         <p>More data</p>
